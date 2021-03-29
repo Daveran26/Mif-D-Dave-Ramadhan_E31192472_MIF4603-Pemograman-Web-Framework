78a1005164d0813065ceb2c7381999941b851aa4
Daveran26 <davezr2426@gmail.com>
2021-03-29 23:50:49 +0800

    summary minggu 8

---
+++ Minggu 8/Praktikum/CI_3/application/views/view_mahasiswa.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    meta http-equiv="X-UA-Compatible" content="IE=edge">
+    E31192472 - Dave Ramadhan Wibisana</title>
+</head>
+<body>
+    <p>Nama : <?= $nama; ?></p>
+    <p>Prodi : <?= $prodi; ?></p>
+</body>
+</html>
